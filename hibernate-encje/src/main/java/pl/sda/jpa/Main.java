b44e54f94a94e0fef1255f0c0a15ad8524d063d8
Your Name <you@example.com>
2018-09-20 23:04:52 +0200

    init

---
+++ hibernate-encje/src/main/java/pl/sda/jpa/Main.java
@@ -0,0 +1,9 @@
+package pl.sda.jpa;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ hibernate-konfiguracja/src/main/java/pl/sda/jpa/Main.java
@@ -0,0 +1,8 @@
+package pl.sda.jpa;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
